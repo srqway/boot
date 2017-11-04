54688c99946097e952294474279cac1e257b8485
hsiehpinghan <hsiehpinghan@localhost>
2017-11-05 00:39:58 +0800

    backup.

---
+++ spring-cloud-config-server-boot/src/main/java/idv/hsiehpinghan/springcloudconfigserverboot/SpringCloudConfigServerBootApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.springcloudconfigserverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerBootApplication.class, args);
+	}
+}
---
+++ spring-cloud-config-server-boot/src/main/java/idv/hsiehpinghan/springcloudconfigserverboot/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.springcloudconfigserverboot.configuration;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigServer
+@Configuration
+public class SpringConfiguration {
+
+}

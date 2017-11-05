a4a2435011f7cdd74a554f48fd8074029b855caa
hsiehpinghan <hsiehpinghan@localhost>
2017-11-06 00:00:21 +0800

    backup.

---
+++ spring-cloud-starter-eureka-server-boot/src/main/java/idv/hsiehpinghan/springcloudstartereurekaserverboot/configuration/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package idv.hsiehpinghan.springcloudstartereurekaserverboot.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class SpringConfiguration {
+
+}

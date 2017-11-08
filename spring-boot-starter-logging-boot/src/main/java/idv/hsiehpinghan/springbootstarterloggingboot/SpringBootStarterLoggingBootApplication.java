834507d286274740188f234dd6f5da18053e2f39
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-11-08 12:29:48 +0800

    backup.

---
+++ spring-boot-starter-logging-boot/src/main/java/idv/hsiehpinghan/springbootstarterloggingboot/SpringBootStarterLoggingBootApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.springbootstarterloggingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterLoggingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterLoggingBootApplication.class, args);
+	}
+}

c507fb2836391e5e2f56f5ef6d5e57072fae29ec
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2017-11-22 18:49:03 +0800

    backup.

--- spring-security-oauth2-boot/src/main/java/idv/hsiehpinghan/springsecurityoauth2boot/configuration/AuthorizationServerSpringConfiguration.java
+++ spring-security-oauth2-boot/src/main/java/idv/hsiehpinghan/springsecurityoauth2boot/configuration/AuthorizationServerSpringConfiguration.java
@@ -1,65 +1,119 @@
 package idv.hsiehpinghan.springsecurityoauth2boot.configuration;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import idv.hsiehpinghan.springsecurityoauth2boot.entity.ClientEntity;
-import idv.hsiehpinghan.springsecurityoauth2boot.entity.GrantedAuthorityEntity;
-import idv.hsiehpinghan.springsecurityoauth2boot.service.ClientService;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerSpringConfiguration extends AuthorizationServerConfigurerAdapter {
-	private static final String DEMO_RESOURCE_ID = "order";
-	
+
+    private int accessTokenValiditySeconds = 10000;
+    private int refreshTokenValiditySeconds = 30000;
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
     @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    RedisConnectionFactory redisConnectionFactory;
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * Configure the non-security features of the Authorization Server endpoints, like token store, token customizations, user approvals and grant types.
+     */
     @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        //配置两个客户端,一个用于password认证一个用于client认证
-        clients.inMemory().withClient("client_1")
-                .resourceIds(DEMO_RESOURCE_ID)
-                .authorizedGrantTypes("client_credentials", "refresh_token")
-                .scopes("select")
-                .authorities("client")
-                .secret("123456")
-                .and().withClient("client_2")
-                .resourceIds(DEMO_RESOURCE_ID)
-                .authorizedGrantTypes("password", "refresh_token")
-                .scopes("select")
-                .authorities("client")
-                .secret("123456");
+    public void configure(AuthorizationServerEndpointsConfigurer authorizationServerEndpointsConfigurer) throws Exception {
+    	authorizationServerEndpointsConfigurer
+                .authenticationManager(this.authenticationManager);
     }
+
+    /**
+     * Configure the security of the Authorization Server, which means in practical terms the /oauth/token endpoint.
+     */
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints
-                .tokenStore(new RedisTokenStore(redisConnectionFactory))
-                .authenticationManager(authenticationManager);
+    public void configure(AuthorizationServerSecurityConfigurer authorizationServerSecurityConfigurer) throws Exception {
+    	authorizationServerSecurityConfigurer
+                // we're allowing access to the token only for clients with 'ROLE_TRUSTED_CLIENT' authority
+                .tokenKeyAccess("hasAuthority('ROLE_TRUSTED_CLIENT')")
+                .checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
     }
+
+    /**
+     * Configure the ClientDetailsService, declaring individual clients and their properties.
+     */
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        //允许表单认证
-        oauthServer.allowFormAuthenticationForClients();
+    public void configure(ClientDetailsServiceConfigurer clientDetailsServiceConfigurer) throws Exception {
+    	clientDetailsServiceConfigurer.inMemory()
+                .withClient("trusted-app")
+                    .authorizedGrantTypes("client_credentials", "password", "refresh_token")
+                    .authorities("ROLE_TRUSTED_CLIENT")
+                    .scopes("read", "write")
+                    .resourceIds(resourceId)
+                    .accessTokenValiditySeconds(accessTokenValiditySeconds)
+                    .refreshTokenValiditySeconds(refreshTokenValiditySeconds)
+                    .secret("secret");
     }
 }
+
+//@Configuration
+//@EnableAuthorizationServer
+//public class AuthorizationServerSpringConfiguration extends AuthorizationServerConfigurerAdapter {
+
+//	@Override
+//	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//		clients.inMemory().withClient("service-account-1").secret("service-account-1-secret")
+//				.authorizedGrantTypes("client_credentials").scopes("resource-server-read", "resource-server-write");
+//	}
+//
+//	@Override
+//	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+//		// 允许表单认证
+//		oauthServer.allowFormAuthenticationForClients();
+//	}
+//}
+
+//@Configuration
+//@EnableAuthorizationServer
+//public class AuthorizationServerSpringConfiguration extends AuthorizationServerConfigurerAdapter {
+//	private static final String DEMO_RESOURCE_ID = "order";
+//	
+//    @Autowired
+//    AuthenticationManager authenticationManager;
+//    @Autowired
+//    RedisConnectionFactory redisConnectionFactory;
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        //配置两个客户端,一个用于password认证一个用于client认证
+//        clients.inMemory().withClient("client_1")
+//                .resourceIds(DEMO_RESOURCE_ID)
+//                .authorizedGrantTypes("client_credentials", "refresh_token")
+//                .scopes("select")
+//                .authorities("client")
+//                .secret("123456")
+//                .and().withClient("client_2")
+//                .resourceIds(DEMO_RESOURCE_ID)
+//                .authorizedGrantTypes("password", "refresh_token")
+//                .scopes("select")
+//                .authorities("client")
+//                .secret("123456");
+//    }
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints
+//                .tokenStore(new RedisTokenStore(redisConnectionFactory))
+//                .authenticationManager(authenticationManager);
+//    }
+//    @Override
+//    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+//        //允许表单认证
+//        oauthServer.allowFormAuthenticationForClients();
+//    }
+//}
 
 //@Configuration
 //@EnableAuthorizationServer
--- spring-security-oauth2-boot/src/main/java/idv/hsiehpinghan/springsecurityoauth2boot/configuration/SecuritySpringConfiguration.java
+++ spring-security-oauth2-boot/src/main/java/idv/hsiehpinghan/springsecurityoauth2boot/configuration/SecuritySpringConfiguration.java
@@ -1,75 +1,72 @@
 package idv.hsiehpinghan.springsecurityoauth2boot.configuration;
 
-import java.util.Arrays;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
-import idv.hsiehpinghan.springsecurityoauth2boot.entity.RoleEntity;
-import idv.hsiehpinghan.springsecurityoauth2boot.entity.UserEntity;
-import idv.hsiehpinghan.springsecurityoauth2boot.service.UserService;
+@Configuration
+@EnableWebSecurity( debug = true )
+public class SecuritySpringConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    	httpSecurity
+                .formLogin().disable() // disable form authentication
+                .anonymous().disable() // disable anonymous user
+                .authorizeRequests().anyRequest().denyAll(); // denying all access
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication() // creating user in memory
+                .withUser("user")
+                    .password("password").roles("USER")
+                .and().withUser("admin")
+                    .password("password").authorities("ROLE_ADMIN");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // provides the default AuthenticationManager as a Bean
+        return super.authenticationManagerBean();
+    }
+}
 
 //@Configuration
 //@EnableWebSecurity
 //public class SecuritySpringConfiguration extends WebSecurityConfigurerAdapter {
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService(){
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withUsername("user_1").password("123456").authorities("USER").build());
-//        manager.createUser(User.withUsername("user_2").password("123456").authorities("USER").build());
-//        return manager;
-//    }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // @formatter:off
-//        http
-//            .requestMatchers().anyRequest()
-//            .and()
-//                .authorizeRequests()
-//                .antMatchers("/oauth/*").permitAll();
-//        // @formatter:on
-//    }
+//	@Autowired
+//	private UserDetailsService userDetailsService;
+//	@Autowired
+//	private UserService userService;
+//
+//	@Override
+//	protected void configure(HttpSecurity httpSecurity) throws Exception {
+//		// for restful service
+//		httpSecurity.csrf().disable();
+//
+//		httpSecurity.authorizeRequests().antMatchers("/oauth/*").permitAll();
+//	}
+//
+//	@PostConstruct
+//	public void postConstruct() {
+//		if (userService.findOne("user") == null) {
+//			UserEntity user = new UserEntity("user", "user", true, true, true, true,
+//					Arrays.asList(new RoleEntity("ROLE_USER", "user role name", null)));
+//			userService.save(user);
+//		}
+//		if (userService.findOne("admin") == null) {
+//			UserEntity admin = new UserEntity("admin", "admin", true, true, true, true,
+//					Arrays.asList(new RoleEntity("ROLE_ADMIN", "admin role name", null)));
+//			userService.save(admin);
+//		}
+//	}
+//
+//	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//		authenticationManagerBuilder.userDetailsService(userDetailsService);
+//	}
 //}
-
-@Configuration
-@EnableWebSecurity
-public class SecuritySpringConfiguration extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
-	@Autowired
-	private UserService userService;
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		// for restful service
-		httpSecurity.csrf().disable();
-
-		httpSecurity.authorizeRequests().antMatchers("/oauth/*").permitAll();
-	}
-
-	@PostConstruct
-	public void postConstruct() {
-		if (userService.findOne("user") == null) {
-			UserEntity user = new UserEntity("user", "user", true, true, true, true,
-					Arrays.asList(new RoleEntity("ROLE_USER", "user role name", null)));
-			userService.save(user);
-		}
-		if (userService.findOne("admin") == null) {
-			UserEntity admin = new UserEntity("admin", "admin", true, true, true, true,
-					Arrays.asList(new RoleEntity("ROLE_ADMIN", "admin role name", null)));
-			userService.save(admin);
-		}
-	}
-
-	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.userDetailsService(userDetailsService);
-	}
-}

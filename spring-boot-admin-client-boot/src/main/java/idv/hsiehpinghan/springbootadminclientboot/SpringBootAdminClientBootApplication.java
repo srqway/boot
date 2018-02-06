1c225def11034038a7619da8c13cb188d832bf20
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-02-06 18:26:48 +0800

    backup.

---
+++ spring-boot-admin-client-boot/src/main/java/idv/hsiehpinghan/springbootadminclientboot/SpringBootAdminClientBootApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.springbootadminclientboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientBootApplication.class, args);
+	}
+}
---
+++ spring-boot-admin-server-boot/src/main/java/idv/hsiehpinghan/springbootadminserverboot/configuration/SpringConfiguration.java
@@ -0,0 +1,11 @@
+package idv.hsiehpinghan.springbootadminserverboot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAdminServer
+public class SpringConfiguration {
+
+}

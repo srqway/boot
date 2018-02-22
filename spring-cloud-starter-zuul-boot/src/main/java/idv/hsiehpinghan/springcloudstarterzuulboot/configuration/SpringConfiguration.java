5d5cba825e6763bd4aa79e1dfb2f824052ce63d8
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-02-22 12:25:34 +0800

    backup.

--- spring-cloud-starter-zuul-boot/src/main/java/idv/hsiehpinghan/springcloudstarterzuulboot/configuration/SpringConfiguration.java
+++ spring-cloud-starter-zuul-boot/src/main/java/idv/hsiehpinghan/springcloudstarterzuulboot/configuration/SpringConfiguration.java
@@ -1,10 +1,12 @@
 package idv.hsiehpinghan.springcloudstarterzuulboot.configuration;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableZuulProxy
+@EnableOAuth2Sso
 public class SpringConfiguration {
 
 }

ed60b6e3f068d28ef933a18c4b0d50093845f910
hsiehpinghan <thank.hsiehpinghan@gmail.com>
2018-11-23 12:10:51 +0800

    backup.

---
+++ spring-cloud-starter-oauth2-boot/redis_token_store_resource_server/src/main/java/idv/hsiehpinghan/springcloudstarteroauth2boot/RedisTokenStoreResourceServerApplication.java
@@ -0,0 +1,12 @@
+package idv.hsiehpinghan.springcloudstarteroauth2boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTokenStoreResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisTokenStoreResourceServerApplication.class, args);
+	}
+}
